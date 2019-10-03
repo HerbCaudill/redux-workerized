b38f1260c9f5b03aee6aa6b15e7284711f2a8722
Herb Caudill <herb@devresults.com>
2019-10-03 20:10:51 +0200

    export public api

--- examples/react/App.tsx
+++ examples/react/App.tsx
@@ -1,5 +1,5 @@
 ﻿import React from 'react'
-import { useDispatch, useSelector } from '../../src/hooks'
+import { useDispatch, useSelector } from '../../src'
 import { actions } from './actions'
 import { CounterState } from './reducer'
 
@@ -8,14 +8,14 @@ export function App() {
   const value = useSelector(valueSelector)
   const dispatch = useDispatch()
 
-  const increment = () => dispatch(actions.increment(1))
+  const increment = () => dispatch(actions.increment(3))
   const decrement = () => dispatch(actions.decrement(2))
 
   return (
     <div>
-      <button onClick={decrement}>-</button>
+      <button onClick={decrement}>-2</button>
       <label>{value}</label>
-      <button onClick={increment}>+</button>
+      <button onClick={increment}>+3</button>
     </div>
   )
 }
