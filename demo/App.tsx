629f3976576241ba16b079d3fdb1dafee0146560
Herb Caudill <herb@devresults.com>
2019-10-03 20:44:38 +0200

    🎈

--- demo/App.tsx
+++ demo/App.tsx
@@ -5,29 +5,22 @@ import { actions } from './actions'
 import { State } from './reducer'
 
 export function App() {
-  const value = useSelector((state: State): number => state.value)
-  const prime = useSelector((state: State): number => state.prime)
+  const prime = useSelector((state: State) => state.prime)
+  const busy = useSelector((state: State) => state.busy)
 
   const dispatch = useDispatch()
 
-  const increment = () => dispatch(actions.increment(1))
-  const decrement = () => dispatch(actions.decrement(1))
-  const nextPrime = () => dispatch(actions.nextPrime())
+  const nextPrime = () => {
+    dispatch(actions.setBusy(true))
+    dispatch(actions.nextPrime())
+  }
 
   return (
     <div>
       <ReactCountdownClock seconds={60} color="teal" size={150} />
-
-      <div style={{ display: 'none' }}>
-        <button onClick={decrement}>-</button>
-        <label>{value}</label>
-        <button onClick={increment}>+</button>
-      </div>
       <div>
-        <p>
-          {prime}
-          <button onClick={nextPrime}>Next prime</button>
-        </p>
+        {prime}
+        <button onClick={nextPrime}>{busy ? 'Calculating...' : 'Next prime'}</button>
       </div>
     </div>
   )
