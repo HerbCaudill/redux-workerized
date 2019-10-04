6d8f4cda50ba4082fe147ad5ae428de1aa52c671
Herb Caudill <herb@devresults.com>
2019-10-04 12:00:11 +0200

    demo/Clock.tsx:

--- demo/Clock.tsx
+++ demo/Clock.tsx
@@ -2,21 +2,24 @@
 import ReactCountdownClock from 'react-countdown-clock'
 
 export const Clock = () => {
-  const [time, setTime] = useState(2)
+  const [time, setTime] = useState(3)
   const [seconds, setSeconds] = useState(time)
-  const reset = () => {
-    const t = time + 0.1
+
+  const restart = () => {
+    // can't just set seconds back to the starting value, or it won't restart
+    const t = time + 0.00000000001
     setTime(t)
     setSeconds(t)
   }
+
   return (
     <ReactCountdownClock
       seconds={seconds}
-      color="teal"
+      color="008080"
       timeFormat="seconds"
       weight={20}
       size={200}
-      onComplete={reset}
+      onComplete={restart}
     />
   )
 }
